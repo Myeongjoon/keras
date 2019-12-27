4c5bc9c95c83cf12ab7fc41a38fb2ccf316b9e5f
Myeongjoon <mgim@ebay.com>
2019-12-27 10:59:19 +0900

    함수 이름 수정

---
+++ mgim/arange_numpy_test.py
@@ -0,0 +1,3 @@
+
+def arange(start, stop=None, step=1, dtype='int32'):
+    return np.arange(start, stop, step, dtype)
--- mgim/cumsum_cntk_test.py
+++ mgim/cumsum_cntk_test.py
@@ -4,9 +4,15 @@ import cntk as C
 
 def cumsum(x, axis=0):
    dim = x.shape[axis]
+   print('dim')
+   print(dim)
    U = C.constant(np.triu(np.ones((dim, dim))).astype(x.dtype))
+   print('U')
+   print(U)
    if axis != -1:
        x = C.swapaxes(x, -1, axis)
+       print('swapped')
+       print(x())
    out = C.times(x, U)
    if axis != -1:
        out = C.swapaxes(out, -1, axis)
@@ -21,5 +27,6 @@ def parse_shape_or_val(shape_or_val):
 a,tensor = parse_shape_or_val((4,2))
 
 print(tensor)
-print(backend.cumsum(tensor))
-print(cumsum(tensor)())
+cumsum_res = cumsum(tensor)
+print('result')
+print(cumsum_res())
