e6389ffad827baa2fa6202494f6f281ecefae8d7
Myeongjoon <mgim@ebay.com>
2019-12-27 09:44:42 +0900

    디버깅 하는 함수 추가

--- mgim/keras_cumsum_test.py
+++ mgim/keras_cumsum_test.py
@@ -1,7 +1,16 @@
 from keras import backend
 import numpy as np
+import cntk as C
 
-
+def cumsum(x, axis=0):
+   dim = x.shape[axis]
+   U = C.constant(np.triu(np.ones((dim, dim))).astype(x.dtype))
+   if axis != -1:
+       x = C.swapaxes(x, -1, axis)
+   out = C.times(x, U)
+   if axis != -1:
+       out = C.swapaxes(out, -1, axis)
+   return out
 
 def parse_shape_or_val(shape_or_val):
     if isinstance(shape_or_val, np.ndarray):
@@ -11,4 +20,6 @@ def parse_shape_or_val(shape_or_val):
 
 a,tensor = parse_shape_or_val((4,2))
 
+print(tensor)
 print(backend.cumsum(tensor))
+print(cumsum(tensor)())
